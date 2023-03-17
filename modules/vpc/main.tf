bbc3a5de9044101aacf7364426e8c2684adb8d6d
unknown <practicedevops60@gmail.com>
2023-03-18 03:52:58 +0530

    edit subnet ip

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
 }
 resource "aws_subnet" "prod-subnet-public-2" {
     vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "eu-central-1b"
     tags = {
